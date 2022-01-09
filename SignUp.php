f82d153bd8f3ba5bd1b4631e05573c0dc8810907
ReemAbdulraouf <76842378+ReemAbdulraouf@users.noreply.github.com>
2022-01-09 23:28:54 +0200

    updating front-end

--- SignUp.php
+++ SignUp.php
@@ -5,48 +5,7 @@
     <!-- <link rel='stylesheet' href='bootstrap.min.css'> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-  <style>
-      body{
-          background-image: url("Images/signUp.jpg");
-          background-size: cover;
-          background-repeat: no-repeat;
-      }
-        input[type="number"]{
-        width:48% !important 
-    }
-    input[name="Emergency Number"]{
-        margin-left: 0.5rem;
-    }
-
-      @media (max-width: 580px) {
-        input[type="number"]{
-        width:100% !important 
-    }
-    input[name="Emergency Number"]{
-        margin-left: 0rem;
-    }
-      }
-    
-    .linkClick{
-        text-decoration: none;
-    }
-
-    .alert{
-        display: none;
-    }
-
-    .form-container{
-        background-color: rgba(255, 255, 255, 0.774);
-    }
-
-    .showBtn{
-        right:15px;
-        top:8px;
-        color: grey;
-        cursor: pointer;
-        z-index: 99999;
-    }
-  </style>
+    <link rel="stylesheet" href="style/login.css">
 
   <script>
     //   var shown = false;
@@ -133,7 +92,7 @@ function validate(form,e){
     <div class="input-group mb-3">
         <span class="input-group-text"><i class="fas fa-lock"></i></span>
         <input type="password" class="form-control" placeholder="Insert your password" name="Password">
-        <div class='position-absolute showBtn' onclick="ShowPassword()"><i class='far fa-eye'></i></div>
+        <div class='showBtn' onclick="ShowPassword()"><i class='far fa-eye'></i></div>
     </div>
 
 
