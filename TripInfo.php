8749a320935a15e1105cbbf5db8adea5f0064e30
SamaRostom <sama1904157@miuegypt.edu.eg>
2022-01-17 01:50:36 +0200

    Update TripInfo.php

--- TripInfo.php
+++ TripInfo.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<style>
         #tripimage{
             width:700px;
@@ -18,7 +18,7 @@
 </head>
 <body>
 <?php
-include "Menu.php";
+include "navbar.php";
 
 if($_SESSION['ID_Type'] == "1"){
 	$servername = "localhost";
@@ -95,6 +95,7 @@ if($_SESSION['ID_Type'] == "1"){
         
 <?php
 }
+include "footer.php";
 ?>	
 
 </body>
