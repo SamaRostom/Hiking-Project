7f0e1d1f4e032f07e5be369f7df9ac31c7d2911e
ReemAbdulraouf <76842378+ReemAbdulraouf@users.noreply.github.com>
2022-01-16 19:21:48 +0200

    Update Profile.php

--- Profile.php
+++ Profile.php
@@ -2,13 +2,36 @@
 <html>
 <head>
 	<title>Profile</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>    
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 	<link rel="stylesheet" href="style/profile.css">
+	<style>
+		/* css in case of a short page */
+		@media (min-width: 995px) {
+		body{
+			height:100vh
+		}
+		.footer{
+			position: absolute;
+			bottom:0;
+		}
+	}
+	</style>
+	<script>
+		function setImage(){
+		var img = document.querySelector('img')
+		if(img.src.endsWith('/')){
+			img.src = 'images/avatar.png'
+		}
+	}
+		</script>
+		
 </head>
-<body>
+<body onload="setImage()">
 	<?php
-	include "Menu.php";
+	// include "Menu.php";
+	include "navbar.php";
 	$servername = "localhost";
         $username = "root";
         $password = "";
@@ -45,5 +68,8 @@
 	</div>
 	
 	</div>
+	<?php include('footer.php') ?>
+
 </body>
+
 </html>
