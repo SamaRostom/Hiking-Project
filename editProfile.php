e2e885d8ba9ebf04ea71836e1a041c3619353497
ReemAbdulraouf <76842378+ReemAbdulraouf@users.noreply.github.com>
2022-01-16 20:19:42 +0200

    Update editProfile.php

--- editProfile.php
+++ editProfile.php
@@ -4,6 +4,21 @@
 	<title>Profile</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+	<style>
+		input{
+			margin-bottom: 10px;
+		}
+		body{
+			background-image: url("images/edit.png");
+			background-size: contain;
+			background-repeat: no-repeat;	
+			background-position-y: bottom;
+			height:100vh;
+		}
+		.container{
+			background-color: #ffffffad;
+		}
+		</style>
 </head>
 <body>
 
@@ -17,21 +32,22 @@
         $conn = new mysqli($servername, $username, $password, $dbname);
 
 
+		echo"<div class='col-10 col-md-8 col-lg-6 container p-4 my-5 mx-auto'>
+		<h1 class='display-6 mb-3'>Edit your Profile</h1>
+	    <form action='' method='post' enctype='multipart/form-data'>
 
-	    echo "<form action='' method='post' enctype='multipart/form-data'>";
-
-		
-
- 	  	echo "Username: <input type= 'text'  name= 'Username'  value=".$_SESSION['Username']."><br>";
-		echo "Email: <input type= 'text'  name= 'Email' value=".$_SESSION['Email']."><br>";
-		echo "City: <input type= 'text'  name= 'City' value=".$_SESSION['City']."><br>";
-		echo "Gender: <input type= 'text'  name= 'Gender' value=".$_SESSION['Gender']."><br>";
-		echo "Phone Number: <input type= 'text'  name= 'Phone_Number' value=".$_SESSION['Phone_Number']."><br>";
-		echo "Emergency Number: <input type= 'text'  name= 'Emergency_Number' value=".$_SESSION['Emergency_Number']."><br>";
-		echo "Profile Picture: <input type= 'file' name= 'Profile_Picture' id ='Profile_Picture'><br>";
-		echo "<input type= 'submit'  name= 'submit'  value= 'Submit' ><br>";
-		echo"</form>";
-
+		Username: <input type= 'text' class='form-control' name= 'Username'  value=".$_SESSION['Username'].">
+		Email: <input type= 'text'  class='form-control' name= 'Email' value=".$_SESSION['Email'].">
+		Password: <input type= 'password' class= 'form-control' name='Password' value=".$_SESSION['Password'].">
+		City: <input type= 'text'  class='form-control' name= 'City' value=".$_SESSION['City'].">
+		Gender: <input type= 'text'  class='form-control' name= 'Gender' value=".$_SESSION['Gender'].">
+		Phone Number: <input type= 'text'  class='form-control' name= 'Phone_Number' value=".$_SESSION['Phone_Number'].">
+		Emergency Number: <input type= 'text'  class='form-control' name= 'Emergency_Number' value=".$_SESSION['Emergency_Number'].">
+		Profile Picture: <input type= 'file'  class='form-control' name= 'Profile_Picture' id ='Profile_Picture'>
+		<div style='text-align:right'><input type='submit' class='btn btn-primary px-5 mt-4' name='submit' value='Save'>
+		<input type='button' class='btn btn-outline-dark px-5 ms-3 mt-4' onclick='history.back();' value='Cancel'></div>
+		</form>
+		</div>";
 
 		if(isset($_POST['submit'])){
 
@@ -43,6 +59,8 @@
 		$_SESSION['Username'] = $Username;
 		$Email = $_POST['Email'];
 		$_SESSION['Email'] = $Email;
+		$Password = $_POST['Password'];
+		$_SESSION['Password'] = $Password;
 		$City = $_POST['City'];
 		$_SESSION['City'] = $City;
 		$Gender = $_POST['Gender'];
@@ -54,7 +72,7 @@
 		$ID_Person = $_SESSION['ID_Person'];
 		
 
-		$sql="UPDATE person SET Username = '$Username', Email ='$Email', City = '$City', Gender = '$Gender', Phone_Number = '$Phone_Number', Emergency_Number = '$Emergency_Number', Profile_Picture = '$filename' WHERE ID_Person='".$_SESSION['ID_Person']."'";
+		$sql="UPDATE person SET Username = '$Username', Email ='$Email', Password ='$Password', City = '$City', Gender = '$Gender', Phone_Number = '$Phone_Number', Emergency_Number = '$Emergency_Number', Profile_Picture = '$filename' WHERE ID_Person='".$_SESSION['ID_Person']."'";
 
 		$result=mysqli_query($conn,$sql);
 		if($result){
@@ -63,4 +81,6 @@
 	}	
 	?>
 </body>
+<?php include('footer.php') ?>
+
 </html>
