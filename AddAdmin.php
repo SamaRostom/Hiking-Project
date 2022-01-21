563fbde3391f5554cc7b9af50abf188bcbb42ef0
SamaRostom <sama1904157@miuegypt.edu.eg>
2022-01-21 15:33:35 +0200

    Update AddAdmin.php

--- AddAdmin.php
+++ AddAdmin.php
@@ -2,15 +2,44 @@
 <html>
 <head>
   <title>Add Admin</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  <link rel="stylesheet" href="style/login.css">
+  <style>
+    body{
+      background-image: url("Images/add.jpg");
+      background-size: cover;
+      background-repeat: no-repeat;
+    }
+  </style>  
   <script>
-function ShowPassword() {
-  var x = document.getElementById("Password");
-  if (x.type === "password") {
-    x.type = "text";
-  } else {
-    x.type = "password";
+  function ShowPassword() {
+        var pass = document.querySelector('input[name="Password"]');
+        var icon = document.querySelector('.showBtn i');
+        if (pass.type === "password") {
+            pass.type = "text";
+            icon.setAttribute('class','far fa-eye-slash');
+        } else {
+            pass.type = "password";
+            icon.setAttribute('class','far fa-eye');
+        }
+	}
+  function validate(form,e){
+    	var fail="";
+      var alertBox = document.querySelector('.alert')
+    	if(form.Email.value =="" || form.Password.value ==""){
+    		fail="Please fill the empty fields";
+    	}
+    	if(fail==""){
+    		return true;
+    	}
+    	else{
+        alertBox.querySelector('span').innerText = fail
+    		alertBox.style.display = 'block';
+            setTimeout(function(){alertBox.style.display = 'none';},5000)
+            e.preventDefault()
+    	}
   }
-}
 </script>
 </head>
 <body>
@@ -31,22 +60,54 @@ if(isset($_POST['Add_Admin'])){
     }
 }
   ?>
-  <center><h1>Add Admin</h1>
-<form method="post" action=""> 
-   <input type="text"  placeholder= "Username" name="Username"><br><br>
-   <input type="text" placeholder= "Email" name="Email"><br><br>
-   <input type="Password" placeholder= "Password" id="Password" name="Password"><br><br>
-   <input type="checkbox" onclick="ShowPassword()">Show Password<br><br>
-   <input type="text" placeholder= "City" name="City"><br>
-   <h3>Choose the gender:</h3>  
-   <input type="radio" name="Gender" <?php if (isset($Gender) && $Gender == "Male") echo "checked";?> value = "Male"> Male
-   <input type="radio" name="Gender" <?php if (isset($Gender) && $Gender == "Female") echo "checked";?> value = "Female"> Female<br><br>
-   <input type="text" placeholder= "Phone Number" name="Phone Number"><br><br>
-   <input type="text" placeholder= "Emergency Number" name="Emergency Number"><br><br>
-   <input type="submit" value="Add" name="Add_Admin">
-   <input type="reset">
+  <div class='col-10 col-md-7 col-lg-6 col-xl-5 m-auto form-container px-5 py-4 my-5'>
+        <h1 class='display-6 text-center'>Add Admin</h1>
+<form action="" method="post" onsubmit="validate(this,event)">
+    <div class="input-group mt-4 mb-3">
+        <span class="input-group-text"><i class="fas fa-user"></i></span>
+        <input type="text" class="form-control" placeholder="Username" name="Username">
+    </div>
+    <div class="input-group mb-3">
+        <span class="input-group-text"><i class="fas fa-at"></i></span>
+        <input type="email" class="form-control" placeholder="Email" name="Email">
+    </div>
+    <div class="input-group mb-3">
+        <span class="input-group-text"><i class="fas fa-lock"></i></span>
+        <input type="password" class="form-control" placeholder="Password" name="Password">
+        <div class='showBtn' onclick="ShowPassword()"><i class='far fa-eye'></i></div>
+    </div>
+    <select class="form-select" name='City'>
+        <option selected disabled hidden>Choose a city</option>
+        <option value="Cairo">Cairo</option>
+        <option value="Alexandria">Alexandria</option>
+        <option value="Luxor">Luxor</option>
+        <option value="Aswan">Aswan</option>
+        <option value="Asyut">Asyut</option>
+        <option value="Sohag">Sohag</option>
+        <option value="Port Said">Port Said</option>
+        <option value="Behira">Behira</option>
+        <option value="Faiyum">Faiyum</option>
+        <option value="Giza">Giza</option>
+        <option value="Ismailia">Ismailia</option>
+        <option value="Kafr El Sheikh">Kafr El Sheikh</option>
+        <option value="Red Sea">Red Sea</option>
+        <option value="Matruh">Matruh</option>
+    </select>
+    <p class='mt-3 mb-2'>Choose the gender:</p>  
+    <div class='mb-2'><input class="form-check-input" type="radio" name="Gender" id='male' value="Male" checked>
+      <label class="form-check-label" for="male">Male</label>
+    </div>
+    <div><input class="form-check-input" type="radio" name="Gender" id='female' value="Female">
+      <label class="form-check-label" for="female">Female</label>
+    </div>
+    <p class='mt-3 mb-2'>Insert contact info:</p>
+    <input type="number" class="form-control d-sm-inline mb-3" placeholder="Personal number" name="Phone Number"> 
+    <input type="number" class="form-control d-sm-inline mb-3" placeholder="Emergency number" name="Emergency Number"> 
+   <div class='mt-4 text-center'>
+    <input type="submit" class='btn btn-primary px-5 mb-3' value="Add" name="Submit">
+   <input type="reset" class='btn btn-outline-dark px-5 ms-2 mb-3'>
+  </div>
 
 </form>
-</center>
 </body>
 </html>
