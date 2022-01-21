88570495a564a478a766946fed7eadd6f2478dff
ReemAbdulraouf <76842378+ReemAbdulraouf@users.noreply.github.com>
2022-01-22 00:11:45 +0200

    Update editProfile.php

--- editProfile.php
+++ editProfile.php
@@ -18,8 +18,30 @@
 		.container{
 			background-color: #ffffffad;
 		}
+		.showBtn{
+position: absolute;
+  right:15px;
+  top:8px;
+  color: rgb(128, 128, 128);
+  cursor: pointer;
+  z-index: 99999;
+}
 		</style>
 </head>
+<script>
+	  function ShowPassword() {
+  var pass = document.querySelector('input[name="Password"]');
+  var icon = document.querySelector('.showBtn i');
+  if (pass.type === "password") {
+    pass.type = "text";
+    icon.setAttribute('class','far fa-eye-slash');
+  } else {
+    pass.type = "password";
+    icon.setAttribute('class','far fa-eye');
+  }
+
+}
+</script>
 <body>
 
 	<?php
@@ -35,16 +57,35 @@
 		echo"<div class='col-10 col-md-8 col-lg-6 container p-4 my-5 mx-auto'>
 		<h1 class='display-6 mb-3 text-center'>Edit your Profile</h1>
 	    <form action='' method='post' enctype='multipart/form-data'>
-
 		Username: <input type= 'text' class='form-control' name= 'Username'  value=".$_SESSION['Username'].">
 		Email: <input type= 'text'  class='form-control' name= 'Email' value=".$_SESSION['Email'].">
-		Password: <input type= 'password' class= 'form-control' name='Password' value=".$_SESSION['Password'].">
-		City: <input type= 'text'  class='form-control' name= 'City' value=".$_SESSION['City'].">
+		Password:
+		<div class='input-group mb-3'>
+		<input type= 'password' class= 'form-control' name='Password' value=".$_SESSION['Password'].">
+		<div class='showBtn' onclick='ShowPassword()'><i class='far fa-eye'></i></div>
+		</div>
+		City:<select class='form-select' name='City'>
+        <option selected disabled hidden>".$_SESSION['City']."</option>
+        <option value='Cairo'>Cairo</option>
+        <option value='Alexandria'>Alexandria</option>
+        <option value='Luxor'>Luxor</option>
+        <option value='Aswan'>Aswan</option>
+        <option value='Asyut'>Asyut</option>
+        <option value='Sohag'>Sohag</option>
+        <option value='Port Said'>Port Said</option>
+        <option value='Behira'>Behira</option>
+        <option value='Faiyum'>Faiyum</option>
+        <option value='Giza'>Giza</option>
+        <option value='Ismailia'>Ismailia</option>
+        <option value='Kafr El Sheikh'>Kafr El Sheikh</option>
+        <option value='Red Sea'>Red Sea</option>
+        <option value='Matruh'>Matruh</option>
+    </select>
 		Gender: <input type= 'text'  class='form-control' name= 'Gender' value=".$_SESSION['Gender'].">
 		Phone Number: <input type= 'text'  class='form-control' name= 'Phone_Number' value=".$_SESSION['Phone_Number'].">
 		Emergency Number: <input type= 'text'  class='form-control' name= 'Emergency_Number' value=".$_SESSION['Emergency_Number'].">
 		Profile Picture: <input type= 'file'  class='form-control' name= 'Profile_Picture' id ='Profile_Picture'>
-		<div style='text-align:right'><input type='submit' class='btn btn-primary px-5 mt-4' name='submit' value='Save'>
+		<div style='text-align:center'><input type='submit' class='btn btn-primary px-5 mt-4' name='submit' value='Save'>
 		<input type='button' class='btn btn-outline-dark px-5 ms-3 mt-4' onclick='history.back();' value='Cancel'></div>
 		</form>
 		</div>";
