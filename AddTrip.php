7ae77fde1c5f78855e2aa579f8ab2defbb715186
SamaRostom <sama1904157@miuegypt.edu.eg>
2022-01-23 17:20:09 +0200

    Update AddTrip.php

--- AddTrip.php
+++ AddTrip.php
@@ -19,6 +19,36 @@
             background-color: #ffffffad;
         }
   </style>
+  <script>
+    function validate(form,e){
+    	var fail="";
+      var alertBox = document.querySelector('.alert')
+      if(form.TripPrice.value ==""){
+    		fail+="Please fill the TripPrice field\n";
+    	}
+    	if(form.StartDate.value ==""){
+    		fail+="Please fill the StartDate field\n";
+    	}
+    	if(form.EndDate.value ==""){
+    		fail+="Please fill the EndDate field\n";
+    	}
+      if(form.DescriptionTrip.value ==""){
+        fail+="Please fill the DescriptionTrip field\n";
+      }
+      if(form.HikingPlace.value ==""){
+        fail+="Please fill the HikingPlace field\n";
+      }
+    	if(fail==""){
+    		return true;
+    	}
+    	else{
+        alertBox.querySelector('span').innerText = fail
+    		alertBox.style.display = 'block';
+            setTimeout(function(){alertBox.style.display = 'none';},5000)
+            e.preventDefault()
+    	}
+    }
+</script>
 </head>
 <body>
 	<?php
@@ -29,13 +59,25 @@ $password = "";
 $dbname = "webproject";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-if(isset($_POST['Trip_Btn'])){
+if(isset($_POST['submit'])){
    $dir="images/";
     $filename=$dir.basename($_FILES['Trip_Img']['name']);
     move_uploaded_file($_FILES['Trip_Img']['tmp_name'], $filename);
 
 	 $sql="INSERT into trips(`Trip_Price`, `Start_Date`, `End_Date`, `Description_Trip`, `City`, `Hiking_Place`, `Trip_Image`) values ('".$_POST['TripPrice']."','".$_POST["StartDate"]."','".$_POST["EndDate"]."','".$_POST["DescriptionTrip"]."','".$_POST["HikingCity"]."','".$_POST["HikingPlace"]."' ,'".$filename."')";
      //echo $sql;
+     try{
+      if($conn->query($sql) === FALSE )
+      throw new Exception("Error: " . $sql . "<br>" . $conn->error);
+    }
+    catch(Exception $e)
+    {
+      echo "Message :",$e->getMessage();
+      $myfile = fopen("error.txt", 'a') or die("Unable to open file!");
+      $txt = "Error: " . $sql . "<br>" . $conn->error;
+      fwrite($myfile, $txt);
+      fclose($myfile);
+    }  
     if($conn->query($sql) === TRUE){
       echo '<script> alert("Trip Added successfully");</script>';
       header("Location:Trips.php");
@@ -45,7 +87,7 @@ if(isset($_POST['Trip_Btn'])){
 	
   <div class='col-10 col-md-8 col-lg-6 container p-4 my-5 mx-auto'>
         <h1 class='display-6 mb-3 text-center'><b>Add Trips</b></h1>
-<form method="post" action="" enctype="multipart/form-data"> 
+<form method="post" action="" onsubmit="validate(this,event)" enctype="multipart/form-data"> 
  The Price: <input type="text" class='form-control' placeholder= "Price" name="TripPrice"><br><br>
 
   The Start Date: <input type="text" class='form-control' placeholder= "start date" name="StartDate"><br><br>
@@ -58,7 +100,11 @@ if(isset($_POST['Trip_Btn'])){
 
    The Hiking Place:  <input type="text" class='form-control' placeholder= "Hiking place" name="HikingPlace"><br><br>
     Image: <input type="file" class='form-control' name="Trip_Img" id="Trip_Img" ><br><br>
-   <div style='text-align:center'><input type='submit' class='btn btn-primary px-5 mt-4' name='submit' value='Add Trip'>
+    <div class="alert alert-warning" role="alert">
+    <i class="fas fa-exclamation-triangle"></i> 
+    <span></span>
+</div>
+    <div style='text-align:center'><input type='submit' class='btn btn-primary px-5 mt-4' name='submit' value='Add Trip'>
         <input type='button' class='btn btn-outline-dark px-5 ms-3 mt-4' onclick='history.back();' value='Reset'></div>
 
  </div>
