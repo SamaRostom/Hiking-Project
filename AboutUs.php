cfdae86d96d5ebe8351d9848968afa3459374b8d
SamaRostom <sama1904157@miuegypt.edu.eg>
2022-01-23 17:19:11 +0200

    Update AboutUs.php

--- AboutUs.php
+++ AboutUs.php
@@ -4,14 +4,10 @@
   <?php
      include "navbar.php";
   ?>
-<meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <style>
  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap');
 
-/* *{
-  box-sizing:border-box;
-} */
 
 .container {
   padding: 64px;
